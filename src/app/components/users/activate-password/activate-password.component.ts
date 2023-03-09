8be8872ce6939814ef57acb53f99ec32589c5c8b
David <CodaPass2000>
2023-03-09 13:27:23 +0100

    Uradjen page za aktiviranje sifre i page za resetovanje sifre zajedno sa api req.

--- src/app/components/users/activate-password/activate-password.component.ts
+++ src/app/components/users/activate-password/activate-password.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PopupComponent} from "../../popup/popup.component";
+import {UserService} from "../../../services/user.service";
+
 
 @Component({
   selector: 'app-activate-password',
@@ -7,9 +12,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivatePasswordComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(PopupComponent)
+  popupComponent!: PopupComponent;
 
-  ngOnInit(): void {
+  passwordEntryForm!: FormGroup;
+  userId!: string | null;
+
+  errorMessage: string = ''
+
+  constructor(private formBuilder: FormBuilder, private activatedRoute: ActivatedRoute, private router: Router, private userService: UserService) {
   }
 
+  ngOnInit(): void {
+    this.passwordEntryForm = this.formBuilder.group({
+      code: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    });
+
+
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.userId = params.get('userId');
+    });
+  }
+
+  onPasswordEntrySubmit() {
+    // Proveriti da li se sifre slazu
+    if (this.passwordEntryForm.value.password !== this.passwordEntryForm.value.confirmPassword) {
+      this.errorMessage = 'Šifre se ne slažu!';
+      return;
+    }
+
+    //Pozivamo backend api da podesimo sifru uz pomoc verifikacionog koda i userId-a
+    if (this.userId != null && this.userId != "" &&
+      this.passwordEntryForm.value.code != null && this.passwordEntryForm.value.code != "" &&
+      this.passwordEntryForm.value.password != null && this.passwordEntryForm.value.password != "") {
+
+      this.userService.activatePassword(this.userId, this.passwordEntryForm.value.code, this.passwordEntryForm.value.password)
+        .subscribe(
+          () => {
+            //this.popupService.showPopup('Password activated successfully!');
+            this.router.navigate(['/']);
+          },
+          error => {
+            this.openPopupExample(error.message);
+          }
+        );
+
+      // Nakon uspesnog unosa sifre redirektovavnje na home
+      this.router.navigate(['/']);
+    } else {
+      this.errorMessage = 'Polja nisu popunjena!';
+    }
+
+  }
+
+  openPopupExample(errorText: string) {
+    this.popupComponent.openPopup(errorText);
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {catchError, Observable, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private usersUrl = environment.usersUrl;
+  private usersUrl = environment.usersUrl + "/reset-password";
   private headers = new HttpHeaders({
-    'Authorization':'Bearer ' + localStorage.getItem("jwt")
+    'Authorization': 'Bearer ' + localStorage.getItem("jwt")
   });
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  activatePassword(id: string, secretKey: string, password: string): Observable<any> {
+    return this.httpClient.post<any>(`${this.usersUrl}/${id}`, {password, secretKey}, {
+      headers: this.headers
+    }).pipe(
+      catchError(err => {
+        return throwError(() => new Error(err.error.message));
+      })
+    );
+  }
 }
