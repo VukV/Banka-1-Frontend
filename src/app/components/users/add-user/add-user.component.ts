06798de5a531a8bf0794674a88847c458788c4ae
Nikola <nikola.dubak@hotmail.com>
2023-03-09 11:03:27 +0100

    Završena stranica za registrovanje korisnika

--- src/app/components/users/add-user/add-user.component.ts
+++ src/app/components/users/add-user/add-user.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {UserPositionEnum} from "../../../model/user-position-enum";
+import {UserRoleEnum} from "../../../model/user-role-enum";
+import {UserService} from "../../../services/user.service";
+import {PopupComponent} from "../../popup/popup.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-user',
@@ -6,10 +11,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
+  @ViewChild(PopupComponent)
+  popupComponent!: PopupComponent;
 
-  constructor() { }
+  emailRegex = new RegExp("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$");
+  jmbgRegex = new RegExp("^[0-9]{13}$");
 
-  ngOnInit(): void {
+  email: string = "";
+  phone: string = "";
+  jmbg: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  position: string = "";
+  allPositions: string[] = Object.values(UserPositionEnum);
+  roles: string[] = [];
+  allRoles: string[] = Object.values(UserRoleEnum);
+  error: string = "Testing error message with some smart error";
+
+  constructor(private userService: UserService,
+              private router: Router) { }
+
+  ngOnInit(): void { }
+
+  roleCheckChanged(role: string, event: Event): void {
+    const isChecked = (event.target as HTMLInputElement).checked;
+    if (isChecked)
+      this.roles.push(role);
+    else
+      this.roles = this.roles.filter(arrayRole => arrayRole !== role);
   }
 
+  addUser(): void {
+    this.error = "";
+    if (!this.emailRegex.test(this.email)) {
+      this.error = "Email nije validan!";
+      return;
+    }
+    if (this.phone == "") {
+      this.error = "Telefon mora biti unet!";
+      return;
+    }
+    if (!this.jmbgRegex.test(this.jmbg)) {
+      this.error = "JMBG nije validan!";
+      return;
+    }
+    if (this.firstName == "") {
+      this.error = "Ime mora biti uneto!";
+      return;
+    }
+    if (this.lastName == "") {
+      this.error = "Prezime mora biti uneto!";
+      return;
+    }
+    if (this.position == "") {
+      this.error = "Pozicija mora biti odabrana!";
+      return;
+    }
+    const position = Object.keys(UserPositionEnum)[Object.values(UserPositionEnum).indexOf(this.position as UserPositionEnum)];
+    const roles = this.roles.map(role => Object.keys(UserRoleEnum)[Object.values(UserRoleEnum).indexOf(role as UserRoleEnum)]);
+    this.userService.addUser(this.email, this.phone, this.jmbg, this.firstName, this.lastName, position, roles)
+      .subscribe({
+          next: () => this.router.navigate(["users"]),
+          error: (error) => this.popupComponent.openPopup(`Registrovanje korisnika nije uspelo: ${error.error.message}`)
+        }
+      );
+  }
+
+  cancel(): void {
+    this.router.navigate(["users"]);
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {UserModel} from "../model/user-model";
 
 @Injectable({
   providedIn: 'root'
@@ -13,4 +15,21 @@ export class UserService {
   });
 
   constructor(private httpClient: HttpClient) { }
+
+  addUser(email: string, phone: string, jmbg: string, firstName: string, lastName: string, position: string, roles: string[])
+  : Observable<UserModel> {
+    const userCreationData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      jmbg: jmbg,
+      phoneNumber: phone,
+      position: position,
+      roles: roles
+    };
+    console.log(userCreationData);
+    return this.httpClient.post<UserModel>(`${this.usersUrl}/api/users/create`, userCreationData, {
+        headers: this.headers
+      });
+  }
 }
