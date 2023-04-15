93316c76ae5078f76f9fd2a2f261c134bb086464
miljanastanic <mstanic5819rn@raf.rs>
2023-04-15 17:39:20 +0200

    "Odradjen izgled komponenti: kompanije, ugovori i porudbine"

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -4,6 +4,7 @@ declare global {
   namespace Cypress {
     interface Chainable {
       login(): Chainable<void>
+      btnClickStock(): Chainable<void>
     }
   }
 }
@@ -15,4 +16,18 @@ Cypress.Commands.add("login", () => {
   cy.get("#userpassword").type(Cypress.env("password"));
   cy.get("[type='submit']").click();
   cy.url().should("equal", Cypress.config().baseUrl);
+})
+
+Cypress.Commands.add("btnClickStock", () => {
+  cy.get('button:contains("5 min")').click();
+  cy.get('button:contains("5 min")').should('have.class', 'active')
+  cy.get('button:contains("Sat")').click()
+  cy.get('button:contains("Sat")').should('have.class', 'active')
+  cy.get('button:contains("Dan")').click()
+  cy.get('button:contains("Dan")').should('have.class', 'active')
+  cy.get('button:contains("Nedelja")').click()
+  cy.get('button:contains("Nedelja")').should('have.class', 'active')
+  cy.get('button:contains("Mesec")').click()
+  cy.get('button:contains("Mesec")').should('have.class', 'active')
+
 })
