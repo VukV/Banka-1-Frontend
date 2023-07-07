d5b67e679a549fc1d955d3471bc66a8780ea61e0
VukV <vuk.vukovic.2000@gmail.com>
2023-07-07 15:09:23 +0200

    env url

--- bank/src/environments/environment.ts
+++ bank/src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: false,
-  usersUrl: "http://localhost:8080/api/users",
-  stocksUrl: "http://localhost:8081/api/stocks",
-  forexUrl: "http://localhost:8081/api/forexes",
-  ordersUrl: "http://localhost:8081/api/orders",
-  capitalUrl: "http://localhost:8080/api/user-listings",
-  optionsUrl: "http://localhost:8081/api/options",
-  companiesUrl: "http://localhost:8083/api/companies",
-  contractsUrl: "http://localhost:8082/api/contracts",
-  contactsUrl: "http://localhost:8083/api/contacts",
-  accountsUrl: "http://localhost:8083/api/accounts"
+  bankUrl: "http://localhost:8074/api/bank"
 };
 
 /*
