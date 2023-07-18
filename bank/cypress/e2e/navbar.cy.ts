ad08012806478314790bb12e304a8cbcc548d48a
VukV <vuk.vukovic.2000@gmail.com>
2023-07-18 16:45:43 +0200

    guard, cypress

--- bank/cypress/e2e/navbar.cy.ts
+++ bank/cypress/e2e/navbar.cy.ts
@@ -1,94 +1,46 @@
-/*
-        describe('Navigacija', () => {
-          it('Da li nas link dobro redirektuje', () => {
-            cy.visit('/http://localhost:4200')
-
-            cy.contains('Berza').click()
-            cy.location('pathname').should('eq', '/')
-            cy.go('back')
-
-            cy.contains('Kapital').click()
-            cy.location('pathname').should('eq', '/capital')
-            cy.go('back')
-
-            cy.contains('Porudžbine').click()
-            cy.location('pathname').should('eq', '/orders')
-            cy.go('back')
-
-            cy.contains('Kompanije').click()
-            cy.location('pathname').should('eq', '/campanies')
-            cy.go('back')
-
-            cy.contains('Ugovori').click()
-            cy.location('pathname').should('eq', '/contracts')
-            cy.go('back')
-
-
-            cy.contains('Korisnici').click()
-            cy.location('pathname').should('eq', '/users')
-            cy.go('back')
-
-
- */
-
 describe('Navigacija linkovi', () => {
   beforeEach(() => {
-    cy.login();
+    cy.loginClient();
     cy.visit('/');
   });
 
-  it('Navigates to Berza', () => {
-    cy.get('[data-cy="stock-market"]').click();
-    cy.url().should('contain', '/stock-market');
+  it('Nastavi na pocetnu', () => {
+    cy.get('[data-cy="continue-home"]').click({force: true});
+    cy.url().should('contain', '/home-page');
   });
 
-  it('Navigates to Kapital', () => {
-    cy.get('[data-cy="capital"]').click();
-    cy.url().should('contain', '/capital');
+  it('Navigacija na pocetnu', () => {
+    cy.get('[data-cy="homepage-link"]').click();
+    cy.url().should('contain', '/home-page');
   });
 
-  it('Navigates to Porudžbine', () => {
+  it('Navigacija na racune', () => {
+    cy.get('[data-cy="accounts-link"]').click();
+    cy.url().should('contain', '/accounts');
+  });
+
+  it('Navigacija na nova placanja', () => {
     cy.get('[data-cy="orders"]').click();
-    cy.url().should('contain', '/orders');
+    cy.get('[data-cy="orders-new-link"]').should("be.visible");
+    cy.get('[data-cy="orders-new-link"]').click();
+    cy.url().should('contain', '/new-payment');
   });
 
-  it('Navigates to Kompanije', () => {
-    cy.get('[data-cy="companies"]').click();
-    cy.url().should('contain', '/companies');
-  });
-
-  it('Navigates to Ugovori', () => {
-    cy.get('[data-cy="contracts"]').click();
-    cy.url().should('contain', '/contracts');
-  });
-
-  it('Navigates to Korisnici', () => {
-    cy.get('[data-cy="users"]').click();
-    cy.url().should('contain', '/users');
-  });
-
-});
-
-
-describe('Profilna slika navigacija', () => {
-  beforeEach(() => {
-    cy.login();
-    cy.visit('/');
-  });
-
-  it('Navigiraj do my-profile stranice', () => {
-    cy.get('[data-cy="my-profile"]').click();
-    cy.url().should('contain', '/my-profile');
+  it('Navigacija na prenos', () => {
+    cy.get('[data-cy="orders"]').click();
+    cy.get('[data-cy="transfers-link"]').should("be.visible");
+    cy.get('[data-cy="transfers-link"]').click();
+    cy.url().should('contain', '/transfer');
   });
 });
 
 
 describe('Logout', () => {
   beforeEach(() => {
-    cy.login()
+    cy.loginClient()
   })
 
-  it('is redirected to the login page on log out', () => {
+  it('Redirektuje na login prilikom logout-a', () => {
     cy.get('[data-cy="logout"]').click()
     cy.url().should('contain', '/login')
   })
@@ -98,20 +50,13 @@ describe('Logout', () => {
 describe('Kontrola pristupa', () => {
   it('Redirekt na login stranu kada nismo logovani ', () => {
 
-    cy.visit('/capital');
+    cy.visit('/transfer');
     cy.url().should('include', '/login');
 
-    cy.visit('/orders');
+    cy.visit('/accounts');
     cy.url().should('include', '/login');
 
-    cy.visit('/companies');
-    cy.url().should('include', '/login');
-
-
-    cy.visit('/contracts');
-    cy.url().should('include', '/login');
-
-    cy.visit('/users');
+    cy.visit('/natural-persons');
     cy.url().should('include', '/login');
   });
 });
